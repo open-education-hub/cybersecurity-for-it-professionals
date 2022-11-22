8c053701f9688e9c7bd62ed7461a3aaea86f35ad
Razvan Deaconescu <razvan.deaconescu@upb.ro>
2022-11-22 15:12:25 +0200

    data/lab: Update sign.sh script in correct-signature support
    Use detached signature.
    
    Signed-off-by: Razvan Deaconescu <razvan.deaconescu@upb.ro>

--- content/chapters/data-protection/practice/support/correct-signature/sign.sh
+++ content/chapters/data-protection/practice/support/correct-signature/sign.sh
@@ -3,5 +3,5 @@
 num=20
 
 for f in $(seq -f "file-%02g.txt" 0 $(($num-1))); do
-    gpg --armor --output $(basename "$f" .txt).sig --sign "$f"
+    gpg --armor --output $(basename "$f" .txt).sig --detach-sign "$f"
 done
